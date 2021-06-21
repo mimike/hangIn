8e0cd15d85de0462310da9c9b4837e0406b4bfbc
Mimi Ke <mimi.g.ke@gmail.com>
2021-06-21 16:21:59 -0500

    seed data

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -6,7 +6,16 @@ import random
 
 def seed_posts():
     pic_list = [
-      "https://i.ibb.co/yB8hDV7/mimicover.jpg",
+      "https://i.ibb.co/2FCbT0F/14231832-10105927845566810-5020567043774599592-o.jpg",
+    "https://i.ibb.co/vsH9Lbk/lisa4.jpg",
+"https://i.ibb.co/LnQmqfV/mandy.jpg",
+"https://i.ibb.co/DbsRWDR/DSC09061.jpg",
+"https://i.ibb.co/F02XTvQ/DSC09110.jpg",
+"https://i.ibb.co/qy5C5H8/DSC09066.jpg",
+"https://i.ibb.co/9vB75Fj/DSC05168.jpg",
+"https://i.ibb.co/g619ZZP/DSC04865.jpg",
+"https://i.ibb.co/6wGvPSb/nancy.jpg",
+"https://i.ibb.co/Zz6BVMz/jenn3.jpg",
       "https://i.ibb.co/0hQMJ6R/april.jpg",
         "https://i.ibb.co/NrYHjCv/april2.jpg",
         "https://i.ibb.co/rtXzcQ0/mimi3.jpg",
